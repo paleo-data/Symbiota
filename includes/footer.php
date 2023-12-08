c0ee89abd84263c16b77c402cbd61908186dda32
Lindsay Walker <ljwalker@users.noreply.github.com>
2023-12-07 17:09:12 -0800

    Initial CSS update test

--- includes/footer.php
+++ includes/footer.php
@@ -3,14 +3,14 @@
 		<a href="https://www.nsf.gov" target="_blank">
 			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" />
 		</a>
-		<a href="http://idigbio.org" target="_blank" title="iDigBio">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" />
-		</a>
 		<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center">
 			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" />
 		</a>
+		<a href="https://paleo-data.github.io/" target="_blank" title="Paleo Data Working Group">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/trilocorn.png" />
+		</a>
 	</div>
-	<p>This project made possible by National Science Foundation Awards <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=" target="_blank">#------</a>.</p>
+	<p>This project made possible by National Science Foundation Awards # <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2324688" target="_blank">2324688</a>, <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2324689" target="_blank">2324689</a>, & <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2324690" target="_blank">2324690</a>.</p>
 	<p>For more information about Symbiota, <a href="https://symbiota.org/docs" target="_blank" rel="noopener noreferrer">read the docs</a> or contact the <a href="https://symbiota.org/contact-the-support-hub/" target="_blank" rel="noopener noreferrer">Symbiota Support Hub</a>.</p>
 	</p>
 	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
