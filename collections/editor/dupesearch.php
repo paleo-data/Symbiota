64d5b35492f3003c0d7c21b09c24422d93603765
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2025-06-30 09:52:57 -0700

    Closes #1367 Adds eventDate2 to dup search (#2655)
    # Issue #1367
    
    # Summary
    Adds eventDate2 to display when searching for duplicates if eventDate2
    is available.
    
    Current Rendering:
    
    If eventDate but no eventDate2
    `eventDate`
    or
    
    If eventDate and eventDate2
    `eventDate - eventDate2`
    
    If no eventDate and a present eventDate2
    `[Date Empty Lang tag] - eventDate2`
    or if verbatimEventDate is available
    `verbatimEventDate - eventDate2`

--- collections/editor/dupesearch.php
+++ collections/editor/dupesearch.php
@@ -261,15 +261,21 @@ if(!$IS_ADMIN){
 							<?php
 							echo '<span title="recordedby">'.($occObj['recordedBy']?$occObj['recordedBy']:'Collector field empty').'</span>';
 							if($occObj['recordNumber']) echo '<span style="margin-left:20px;" title="recordnumber">'.$occObj['recordNumber'].'</span>';
-							if($occObj['eventDate']){
-								echo '<span style="margin-left:20px;" title="eventdate">'.$occObj['eventDate'].'</span>';
+
+							$dateTitle = 'eventDate';
+
+							if(!$occObj['eventDate'] && $occObj['verbatimEventDate']) {
+								$dateTitle = 'verbatimeventdate';
 							}
-							elseif($occObj['verbatimEventDate']){
-								echo '<span style="margin-left:20px;" title="verbatimeventdate">'.$occObj['verbatimEventDate'].'</span>';
+
+							$dateDisplay = $occObj['eventDate'] ?? $occObj['verbatimEventDate'] ?? $LANG['DATE_EMPTY'];
+							echo '<span style="margin-left:20px;">';
+							echo '<span style="margin-left:20px;" title="' . $dateTitle.'">'. $dateDisplay .'</span>';
+							if($occObj['eventDate2']) {
+								echo ' - <span title="eventDate2">' . $occObj['eventDate2'] . '<span>';
 							}
-							else{
-								echo '<span style="margin-left:20px;" title="eventdate">'.$LANG['DATE_EMPTY'].'</span>';
-							}
+							echo '</span>';
+
 							if($occObj['associatedCollectors']) echo '<div style="margin-left:10px;" title="associatedCollectors">'.$LANG['ASSOC_COLL'].': '.$occObj['associatedCollectors'].'</div>';
 							?>
 						</div>
